dfe21350bb66a9578e4ccee873152de61446134d
taral.bytes <taral.nagar@bytestechnolab>
2023-10-06 17:43:47 +0530

    Added Seevice Provider for the package

---
+++ src/ExceptionHandlerServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BytesTechnoLab\ExceptionHandler;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExceptionHandlerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register your package's services or bindings here, if any.
+    }
+
+    public function boot()
+    {
+        // This is where you can register your custom exception handling.
+        // For example, you can override Laravel's default exception handler.
+        $this->app->singleton(\Illuminate\Contracts\Debug\ExceptionHandler::class, CustomExceptionHandler::class);
+    }
+}
